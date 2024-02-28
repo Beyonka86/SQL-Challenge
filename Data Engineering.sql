8303db36344f6d986b7ce4a6f6cf23a64220fcfe
Beyonka Powell <beyonka86@gmail.com>
2024-02-28 14:05:25 -0600

    Final Push of Module 9

---
+++ Data Engineering.sql
@@ -0,0 +1,126 @@
+-- Create a new table
+CREATE TABLE titles (
+  title_id VARCHAR NOT NULL PRIMARY KEY,
+  title VARCHAR NOT NULL
+);
+
+-- View the table data
+SELECT * FROM titles
+
+CREATE TABLE employees (
+  	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY(emp_title) REFERENCES titles (title_id)
+);
+
+-- View the table data
+SELECT * FROM employees
+
+-- Create a new table
+CREATE TABLE departments (
+  dept_no VARCHAR  NOT NULL PRIMARY KEY,
+  dept_name VARCHAR NOT NULL
+);
+
+-- View the table data
+SELECT * FROM departments
+
+-- Create a new table
+CREATE TABLE dept_emp (
+ 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
+);
+
+-- View the table data
+SELECT * FROM dept_emp
+
+-- Create a new table
+CREATE TABLE dept_manager (
+ 	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
+);
+
+-- View the table data
+SELECT * FROM dept_manager
+
+-- Create a new table
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
+-- View the table data
+SELECT * FROM salaries
+
+-- List the employee number, last name, first name, sex, and salary of each employee.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE  hire_date BETWEEN '1/1/1986' AND '12/31/1986'
+ORDER BY  hire_date;
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
+--List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT departments.dept_name, employees.last_name, employees.first_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' 
+OR departments.dept_name = 'Development';
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
